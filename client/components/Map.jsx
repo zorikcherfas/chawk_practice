e1fcb20c29a58cd1544f4403f70f92da61973030
Zorik Cherfas <zorik.cherfas@gmail.com>
2015-10-23 22:14:33 +0300

    Done part 4

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -10,9 +10,63 @@ Map = React.createClass({
 })
 
 Sidenav = React.createClass({
+  getInitialState() {
+  return {
+    showTooltip: false,
+    tooltipX: "50px",
+    tooltipY: "0px"
+  }
+},
+showTooltip(e) {
+  this.setState({
+    showTooltip: true,
+    tooltipY: e.nativeEvent.target.offsetTop + 
+      (e.nativeEvent.target.offsetHeight / 2) + "px"
+  })
+},
+hideTooltip(e) {
+  this.setState({
+    showTooltip: false
+  })
+},
   render() {
 
-     let iconList = [
+
+   return (
+    <nav className="sidenav">
+          <SidenavTooltip />
+
+      <ul className="sidenav-list">
+      <SidenavIcons />
+
+      </ul>
+    </nav>
+  )
+  }
+})
+
+SidenavTooltip = React.createClass({
+  render() {
+    tooltipStyle = {
+
+    }
+    return (
+      <div className="sidenav-tooltip" style={tooltipStyle}>
+        <p>Data Layer</p>
+        <div className="tail"></div>
+      </div>
+    )
+  }
+})
+
+
+SidenavIcons = React.createClass({
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.id !== this.props.id
+  },
+  render() {
+
+         let iconList = [
     "fa fa-database",
     "fa fa-user-plus",
     "fa fa-users",
@@ -23,21 +77,17 @@ Sidenav = React.createClass({
     "fa fa-line-chart",
     "fa fa-cog"
   ]
-
-  let list = iconList.map((item) => {
-  return (
-    <li key={item} className="sidenav-list-item">
-      <i className={item}></i>
-    </li>
-  )
-  })
-
-   return (
-    <nav className="sidenav">
-      <ul className="sidenav-list">
+    let list = iconList.map((item) => {
+      return (
+        <li key={item} className="sidenav-list-item">
+          <i className={item}></i>
+        </li>
+      )
+    })
+    return (
+      <div>
         {list}
-      </ul>
-    </nav>
-  )
+      </div>
+    )
   }
 })
