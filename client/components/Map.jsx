2dc645dde58dd7a972bee29c5b667a2212354ab7
Zorik Cherfas <zorik.cherfas@gmail.com>
2015-10-23 21:55:20 +0300

    Done part 3

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,7 +1,43 @@
 Map = React.createClass({
   render() {
     return (
-      <h1>This is where the map goes</h1>
+    	<div>
+      		<h1>This is where the map goes</h1>
+      		<Sidenav />
+      	</div>
     )
+  }
+})
+
+Sidenav = React.createClass({
+  render() {
+
+     let iconList = [
+    "fa fa-database",
+    "fa fa-user-plus",
+    "fa fa-users",
+    "fa fa-bicycle",
+    "fa fa-list-ul",
+    "fa fa-lightbulb-o",
+    "fa fa-list-ol",
+    "fa fa-line-chart",
+    "fa fa-cog"
+  ]
+
+  let list = iconList.map((item) => {
+  return (
+    <li key={item} className="sidenav-list-item">
+      <i className={item}></i>
+    </li>
+  )
+  })
+
+   return (
+    <nav className="sidenav">
+      <ul className="sidenav-list">
+        {list}
+      </ul>
+    </nav>
+  )
   }
 })
